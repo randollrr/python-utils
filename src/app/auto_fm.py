3aa6c36c48319fd975288bf5a87fb04f90a54c39
Randoll Revers <randollr@ornu.com>
2023-03-28 19:52:07 +0000

    added support/update for new functionalities cd(), del_dir(), fullpath(), ls(), mkdirs(), oldest(), reset() set_bucket(), set_returns(), _ts_sorted_file()

--- src/app/auto_fm.py
+++ src/app/auto_fm.py
@@ -10,26 +10,18 @@ __version__ = '2.5.0'
 
 class FileManager:
     """
-    Utility (interface) to facilitate REST-based file-system management. Implements
-    methods to manipulate file(s) with common automation operations.
+    Utility (interface) to facilitate REST-based file-system management. Also
+    implements methods to manipulate file(s) with common automation operations.
     """
 
     def __init__(self, indir=None, outdir=None, arcdir=None, errdir=None, \
-        known_dir=None, bucket=None):
-        self._bucket = bucket if bucket else ''
-        self._basedir = f"{envar_in('((env:PWD))')}"
-        # self.filename = None
-        self.known_dir = known_dir
-
-        self.ARCHIVE = arcdir
-        self.ERRORED = errdir
-        self.INPUT = indir
-        self.OUTPUT = outdir
+        known_dir=None, bucket=None) -> None:
+        self.reset(indir, outdir, arcdir, errdir, known_dir, bucket)
 
     def cd(self, newpath) -> Status:
         s = Status(204, 'Nothing happened')
         n, s_fp = self.fullpath(newpath)
-        if s_fp == 200:
+        if s_fp.code == 200:
             self._basedir = n
             s.code = 200
             s.message = f"new directory: {n}"
@@ -47,13 +39,14 @@ class FileManager:
         r = False
         if self.exists(path):
             try:
-                if self._basedir == f"{path}/fm":
-                    os.rmdir(self._basedir)  # remove "/fm/" 1st before "dirstruct"
+                if self.pwd() == f"{path}/fm":
+                    os.rmdir(self.pwd())  # remove "/fm/" 1st before "dirstruct"
                 if self._bucket and self._bucket in path:
                     # os.rmdir(path)
                     # path = path.replace(self._bucket, '')
                     log.warn(f"fm.del_dir(): Cannot delete {self._bucket} in path provided. "
                              f"Use fm.del_bucket() instead.")
+                    return r
                 os.rmdir(path)
                 r = True
                 log.info(f"remove dir: {path}")
@@ -105,6 +98,7 @@ class FileManager:
             elif _p == 'output':
                 self.OUTPUT = _np
 
+        path, _ = self.fullpath(path)
         if path:
             self._basedir = f"{path}/fm"
         if not self.known_dir:
@@ -161,7 +155,10 @@ class FileManager:
             elif '..'  in path:
                 r = upone(path)
             elif path.startswith('.'):
-                r = path.replace('.', f"{_wd}")
+                if len(path) == 1 or (len(path) > 1 and path[1] == '/'):
+                    r = path.replace('.', f"{_wd}")
+                else:
+                    r = f"{_wd}/{path}"
             elif path.endswith('/.'):
                 r = path[:-2]
             elif path and not path.startswith('/'):
@@ -171,7 +168,8 @@ class FileManager:
             s.message = 'Parsing attempted (successfully).'
         return r, s
 
-    def latest(self, directory=None, fn_pattern=None, fn_only=False, ret='list'):
+    def latest(self, directory=None, fn_pattern=None, fn_only=False, path=None,
+               ret=None):
         """
         See ts_based_file().
         :param directory: provide path
@@ -179,14 +177,19 @@ class FileManager:
         :param fn_only: returns list [] of filenames only
         :return: directory, filename, timestamp
         """
+        if not ret:
+            ret = self._output_fmt
+        if path and not directory:
+            directory = path
         r = self._ts_sorted_file(
-            'latest', directory=directory, fn_pattern=fn_pattern, fn_only=fn_only,
-            ret='list')
+            'latest', directory=directory, fn_pattern=fn_pattern,
+            fn_only=fn_only, ret=ret)
         if fn_only and ret == 'list' and len(r) > 0:
             return r[0]
         return r
 
-    def ls(self, directory=None, fn_pattern=None, fn_only=False, ret='list'):
+    def ls(self, directory=None, fn_pattern=None, fn_only=False, path=None,
+           ret=None):
         """
         See ts_based_file().
         :param directory: provide path
@@ -194,9 +197,27 @@ class FileManager:
         :param fn_only: returns list [] of filenames only
         :return: directory, filename, timestamp
         """
+        if not ret:
+            ret = self._output_fmt
+        if path and not directory:
+            directory = path
         return self._ts_sorted_file(
             'list', directory=directory, fn_pattern=fn_pattern, fn_only=fn_only,
-            ret='list')
+            ret=ret)
+
+    def mkdirs(self, path) -> Status:
+        s = Status(204, 'Nothing happened.')
+        path = self.fullpath(path)
+        if path:
+            try:
+                os.makedirs(path, exist_ok=True)
+                s.code = 200
+                s.message = 'OK'
+            except Exception as e:
+                s.code = 500
+                s.message = f"fm.mkdirs() : Error ocurred. : {e}"
+                log.error(s.message)
+        return s
 
     def move(self, fn, src, dst):
         """
@@ -210,28 +231,28 @@ class FileManager:
 
         def newfilename(fnum):  # -- add <filename>_1[+n]
             if fnum == 0:
-                ret = fn
+                _r = fn
             else:
                 try:
                     nfn, ext = fn.split('.')
-                    ret = f"{nfn}_{fnum}.{ext}"
+                    _r = f"{nfn}_{fnum}.{ext}"
                 except Exception as e:
-                    ret = f"{fn}_{fnum}"
+                    _r = f"{fn}_{fnum}"
                     log.error(f"Error: {e}")
-            return ret
+            return _r
 
         def do_move(fnum=0):
-            ret = False
+            _r = False
             new_fn = newfilename(fnum)
             if not self.exists(f"{dst}/{new_fn}"):
                 if self.exists(f"{src}/{fn}"):
                     os.rename(f"{src}/{fn}", f"{dst}/{new_fn}")
                     log.info(f"moved: [{src}/{fn}] to [{dst}/{new_fn}]")
-                    ret = True
+                    _r = True
             else:
                 fnum += 1
-                ret = do_move(fnum)
-            return ret
+                _r = do_move(fnum)
+            return _r
 
         # -- move files (recursively avoid name conflict)
         if src and dst:
@@ -239,7 +260,8 @@ class FileManager:
 
         return r
 
-    def oldest(self, directory=None, fn_pattern=None, fn_only=False, ret='list'):
+    def oldest(self, directory=None, fn_pattern=None, fn_only=False, path=None,
+               ret=None):
         """
         See ts_based_file().
         :param directory: provide path
@@ -247,9 +269,13 @@ class FileManager:
         :param fn_only: returns list [] of filenames only
         :return: directory, filename, timestamp
         """
+        if not ret:
+            ret = self._output_fmt
+        if path and not directory:
+            directory = path
         r = self._ts_sorted_file(
             'oldest', directory=directory, fn_pattern=fn_pattern, fn_only=fn_only,
-            ret='list')
+            ret=ret)
         if fn_only and ret == 'list' and len(r) > 0:
             return r[0]
         return r
@@ -259,6 +285,21 @@ class FileManager:
         Returns current working directory
         """
         return self._basedir
+
+    def reset(self, indir=None, outdir=None, arcdir=None, errdir=None, \
+        known_dir=None, bucket=None) -> None:
+        """
+        Reset all state.
+        """
+        self._bucket = bucket if bucket else ''
+        self._basedir = f"{envar_in('((env:PWD))')}"
+        self.known_dir = known_dir
+        self._output_fmt = 'list'
+        self.ARCHIVE = arcdir
+        self.ERRORED = errdir
+        self.INPUT = indir
+        self.OUTPUT = outdir
+        return
 
     def retainer(self, directory, fn, retain) -> None:
         """
@@ -288,7 +329,7 @@ class FileManager:
 
     @deprecated
     def setbucket(self, dirname) -> None:
-        """Deprecated to follow snake case (better)."""
+        """Deprecated to follow func naming convention."""
         self.set_bucket(dirname)
         return
 
@@ -307,6 +348,14 @@ class FileManager:
                      f'Currently set to "{self._bucket}"')
         return
 
+    def set_returns(self, ret='dict') -> None:
+        """
+        Set data output format to 'list', 'dict' or 'json' (default value: dict).
+        """
+        if ret:
+            self._output_fmt = ret
+        return
+
     def touch(self, fn, time=None) -> None:
         with open(fn, 'a') as f:
             os.utime(f.name, time)
@@ -317,7 +366,7 @@ class FileManager:
         return self._ts_sorted_file(req, directory, fn_pattern, fn_only)
 
     def _ts_sorted_file(self, req='latest', directory=None, fn_pattern=None,
-                        fn_only=False, ret='list'):
+                        fn_only=False, ret=None):
         """
         Look for the latest or oldest modified date from files in directory.
         :param action: 'latest' or 'oldest'
@@ -330,39 +379,70 @@ class FileManager:
         """
         r = []
 
+        if not ret:
+            ret = self._output_fmt
         if not directory:
-            directory = self._basedir
+            directory = self.pwd()
         directory, _ = self.fullpath(directory)
 
         # -- build file list
         log.debug(f"directory: {directory}")
-        log.debug(f"directory exists: {self.exists(directory)}")
+        log.debug(f"path-exists: {'yes' if self.exists(directory) else 'no'}")
         if directory and self.exists(directory):
             t = []
-            f_list = sorted([[int(os.stat(os.path.join(directory, f)).st_ctime), f] \
-                for f in os.listdir(directory)])
-            if fn_pattern:
-                for n in sorted(f_list):
-                    if re.search(fn_pattern, n[1]):
-                        t += [[n[0], n[1]]]
-            elif f_list:
-                t = f_list
-            del f_list
 
-            # -- apply filter
-            if req == 'list':
-                r = t
-            elif req == 'latest' and t:
-                r = [t[len(t)-1][0], t[len(t)-1][1]]
-            elif req == 'oldest' and t:
-                r = [t[0][0], t[0][1]]
-            del t
+            # f_list = sorted([[int(os.stat(os.path.join(directory, f)).st_ctime), f] \
+            #     for f in os.listdir(directory)])
 
-            # -- return simplified list
-            if fn_only and ret == 'list' and r:
-                if req in ['latest', 'oldest']:
-                    r = [r]
-                r = [x[1] for x in r]
+            f_list = []
+            for f in os.scandir(directory):
+                ctime = int(os.stat(os.path.join(directory, f.name)).st_ctime)
+                if ret == 'list':
+                    f_list += [[ctime, f.name]]
+                else:
+                    f_list += [{
+                        'filename': f.name,
+                        'is_dir': True if f.is_dir(follow_symlinks=True) else False,
+                        'is_symlink': True if f.is_symlink() else False,
+                        'size': f.stat().st_size,
+                        'stats': {
+                            'atime': int(f.stat().st_atime),
+                            'ctime': int(f.stat().st_ctime),
+                            'mtime': int(f.stat().st_mtime)}
+                    }]
+
+            if not ret == 'list':
+                if f_list:
+                    f_list.sort(key=lambda x: x['filename'])
+                if ret == 'json':
+                    r = json.dumps(f_list, indent=4)
+                else:
+                    r = f_list
+            else:
+                f_list = sorted(f_list)
+
+                if fn_pattern:
+                    for n in sorted(f_list):
+                        if re.search(fn_pattern, n[1]):
+                            t += [[n[0], n[1]]]
+                elif f_list:
+                    t = f_list
+                del f_list
+
+                # -- apply filter
+                if req == 'list':
+                    r = t
+                elif req == 'latest' and t:
+                    r = [t[len(t)-1][0], t[len(t)-1][1]]
+                elif req == 'oldest' and t:
+                    r = [t[0][0], t[0][1]]
+                del t
+
+                # -- return simplified list
+                if fn_only and ret == 'list' and r:
+                    if req in ['latest', 'oldest']:
+                        r = [r]
+                    r = [x[1] for x in r]
         return r
 
 fm = FileManager()
