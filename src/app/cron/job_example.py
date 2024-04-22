fb50d9fb1651b33fa4cfdc5b334b13ab1eab01de
Randoll Revers <Randoll.Revers@cox.com>
2024-04-22 17:53:06 +0000

    updated

--- src/app/cron/job_example.py
+++ src/app/cron/job_example.py
@@ -1,21 +1,27 @@
 from common.utils import log, Status
 
 
-def run(params:list[tuple]=None) -> tuple[dict, Status]:
+def run(params:dict=None) -> tuple[dict, Status]:
     """
     Running job.
     :param params: list of tuples with key-value pairs. e.g. [('key', 'value'), ...]
     :return: tuple with dictionary and status. e.g. ({'key': 'value'}, Status(200, 'message'))
     """
-    fn = '[job_example.run]'
+    fn = '[cron.job_example.run]'
     s = Status(204, f"No data.")
 
     log.info(f"{fn} running...")
 
     if params:
         log.debug(f"{fn} : received parameters: {params}")
+    res = True
 
     log.info(f"{fn} done.")
-    s.code = 200
-    s.message = f"job_example ran successfully."
+    if res:
+        s.code = 200
+        s.message = f"job_example ran successfully."
     return s
+
+
+if __name__ == "__main__":
+    run()
