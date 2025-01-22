c8f756b18a4041679e9ab2d579c49592cb2c0a69
Randoll Revers <randoll.revers@cox.com>
2025-01-22 19:17:34 +0000

    updated

--- src/test/test_scheduler.py
+++ src/test/test_scheduler.py
@@ -11,7 +11,12 @@ def test_get_list():
     assert res is not None
 
 
-def test_get_mod():
+def test_get_list():
+    print(sch._config_get_list())
+    assert True
+
+
+def test_get_module():
     res = sch.get_module('cron.job_example')
     assert res is not None
 
@@ -25,10 +30,34 @@ def test_get_next_event(req, ret):
     assert res == ret
 
 
+def test_get_next_event_2():
+    print(sch.get_next_event('* * * * *'))
+    print(sch.get_next_event('8 * * * *'))
+    assert True
+
+
+def test_isexecutable():
+    res = sch.isexecutable(sch.get_next_event('* * * * *'))
+    print(res)
+    assert True
+
+
+@pytest.mark.skip('manual-run')
+def test_run_job_1():
+    sch.run_job('ep_alerts.pa_job')
+    assert True
+
+
 @pytest.mark.parametrize('job, params, ret', [
     # (None, None, 200),
     ('cron.job_example', {'cron.job_example':{'say': 'hello world'}}, 200),
 ])
-def test_run_job(job, params, ret):
+def test_run_job_2(job, params, ret):
     res = sch.run_job(job, params)
     assert res[0].code == ret
+
+
+@pytest.mark.skip('manual-run')
+def test_wakeup():
+    print(sch.wakeup())
+    assert True
